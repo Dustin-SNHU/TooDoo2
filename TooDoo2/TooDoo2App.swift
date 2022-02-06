53d280e0ea40b13a66a83ede7ffa6d23b32685b8
Dustin-SNHU <dustin.richardson@snhu.edu>
2022-02-06 10:56:17 -0500

    Initial Commit

---
+++ TooDoo2/TooDoo2App.swift
@@ -0,0 +1,17 @@
+//
+//  TooDoo2App.swift
+//  TooDoo2
+//
+//  Created by Dustin Richardson on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TooDoo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
