b983f2410c1e67171495189633103d53a1dfa17d
Dustin-SNHU <dustin.richardson@snhu.edu>
2022-02-06 15:00:42 -0500

    Added array. Updated ListView to include items in the array using a ForEach loop. Prepared views for Navigation Bar Items.

--- TooDoo2/Views/ListView.swift
+++ TooDoo2/Views/ListView.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct ListView: View {
+    
+    // Array for task items
+    @State var items: [String] = [
+        "Task 1",
+        "Task 2",
+        "Task 3"
+    ]
+    
     var body: some View {
         List { // Creates a list object
-            ListRowView(title: "Title") // Extracted structure for to do list row
+            ForEach(items, id: \.self, content: { item in // Loop to pull in task items specified in the items array
+                ListRowView(title: item) // Add item to task list (repeats in loop)
+            })
         }
+        .listStyle(PlainListStyle()) // Change list style to default android look
         .navigationTitle("TooDoo2") // Navigation title with application name
+        .navigationBarItems( // Navigation items at top of application window
+            leading: EditButton(), // Edit Button
+            trailing: NavigationLink("Add", destination: Text("Destination")) // Add button
+        )
     }
 }
 
@@ -23,5 +38,3 @@ struct ListView_Previews: PreviewProvider {
         }
     }
 }
-
-
