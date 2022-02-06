0682a2213c8ab579e6edbaadaeff7757a0bc408f
Dustin-SNHU <dustin.richardson@snhu.edu>
2022-02-06 16:22:22 -0500

    Updated PreviewProvider to show list items. Changed circles to check whether or not a task has been completed and the colors of the checkmark image.

--- TooDoo2/Views/ListRowView.swift
+++ TooDoo2/Views/ListRowView.swift
@@ -10,19 +10,31 @@ import SwiftUI
 // ListRowView Structure
 struct ListRowView: View {
     
-    let title: String
+    let item: ItemModel
     
     var body: some View {
         HStack { // Horizontal stack
-            Image(systemName: "checkmark.circle") // Insert check mark next to task item
-            Text(title) // Text for task item
+            Image(systemName: item.isCompleted ? "checkmark.circle" : "circle") // Insert check mark next to task item if it has been completed
+                .foregroundColor(item.isCompleted ? .green : .gray) // Set checkmark object to green if completed or gray if not completed
+            Text(item.title) // Text for task item
             Spacer() // Pushes text to the left of the screen
         }
+        .padding(.vertical, 8) // Padding between task items
     }
 }
 
+// Preview Provider
 struct ListRowView_Previews: PreviewProvider {
+    
+    // Temporary items used for preview testing
+    static var item1 = ItemModel(title: "Task 1", isCompleted: false)
+    static var item2 = ItemModel(title: "Task 2", isCompleted: true)
+    
+    // View for both temporary items
     static var previews: some View {
-        ListRowView(title: "Title")
+        Group {
+            ListRowView(item: item1)
+            ListRowView(item: item2)
+        }.previewLayout(.sizeThatFits) // Removes the need for both iOS devices to show up in the preview provider
     }
 }
